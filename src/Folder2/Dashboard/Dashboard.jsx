56eae7c7d617e46b6d6899eb0a39957bb862511c
naimur2001 <naimurrahman18thapril@gmail.com>
2023-06-08 20:01:00 +0600

    working on dashboard

--- src/Folder2/Dashboard/Dashboard.jsx
+++ src/Folder2/Dashboard/Dashboard.jsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
   <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
   <div className="drawer-content flex flex-col items-center justify-center">
     {/* Page content here */}
-    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open SideBar</label>
   
   </div> 
   <div className="drawer-side">
