af4ed2df74a123ee4a4de8b9ba259070ed7cd242
naimur2001 <naimurrahman18thapril@gmail.com>
2023-06-14 12:13:01 +0600

    approved claasess done

--- src/Folder2/Class.jsx
+++ src/Folder2/Class.jsx
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import useClasses from '../Folder1/Hooks/useClasses';
 import ClassCard from './ClassCard';
 import SectionTitle from './Shared/SectionTitle';
 import usePayments from '../Folder1/Hooks/usePayments';
 
 const Class = () => {
-  const [classes]=useClasses();
-  const [paydata]=usePayments();
+  const [classes] = useClasses();
+  const [paydata] = usePayments();
+  const [approvedClasses, setApprovedClasses] = useState([]);
 
+  useEffect(() => {
+    const filteredClasses = classes.filter(cl => cl.status === 'approved');
+    setApprovedClasses(filteredClasses);
+  }, [classes]);
 
-  
   return (
-    <div >
-      <SectionTitle title={'What we Teach -'} > </SectionTitle>
+    <div>
+      <SectionTitle title={'What we Teach -'}></SectionTitle>
       <div className='max-w-5xl mx-auto my-3'>
-      <div className='grid grid-cols-3 gap-3'>
-        {
-          classes?.map(cl=> <ClassCard key={cl._id} cl={cl} ></ClassCard> )
-        }
+        <div className='grid grid-cols-3 gap-3'>
+          {approvedClasses?.map(cl => (
+            <ClassCard key={cl._id} cl={cl}></ClassCard>
+          ))}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
